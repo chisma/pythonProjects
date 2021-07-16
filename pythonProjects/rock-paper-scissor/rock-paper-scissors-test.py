5e4070364ea4cd88b668c52921077077f69beec5
Soumya Prasad <soumya.saligram@gmail.com>
2021-07-16 16:15:08 +0200

    Modified tests, refactored game to remain in loop if wrong input is entered

--- pythonProjects/rock-paper-scissor/rock-paper-scissors-test.py
+++ pythonProjects/rock-paper-scissor/rock-paper-scissors-test.py
@@ -1,10 +1,46 @@
 import unittest
-from rock_paper_scissor import game_play
+import rock_paper_scissor
 
 
 class TestRockPaperScissorMethod(unittest.TestCase):
-    def test_player_wins_with_rock(self):
-        self.assertEqual(game_play('r', 's'), "You won!!!\nComputer played s!")
+    def test_player_wins_rock(self):
+        self.assertEqual(rock_paper_scissor.game_play(
+            'r', 's'), "You won!!!\nComputer played s!")
+
+    def test_player_loses_rock(self):
+        self.assertEqual(rock_paper_scissor.game_play(
+            'r', 'r'), "It's a tie!!!Computer also played r!")
+
+    def test_player_tie_rock(self):
+        self.assertEqual(rock_paper_scissor.game_play(
+            'r', 'p'), "You lost!!!\n\\\\\\\\Computer played p!\\\\\\\\")
+
+    def test_player_wins_paper(self):
+        self.assertEqual(rock_paper_scissor.game_play(
+            'p', 'r'), "You won!!!\nComputer played r!")
+
+    def test_player_tie_paper(self):
+        self.assertEqual(rock_paper_scissor.game_play(
+            'p', 'p'), "It's a tie!!!Computer also played p!")
+
+    def test_player_loses_paper(self):
+        self.assertEqual(rock_paper_scissor.game_play(
+            'p', 's'), "You lost!!!\n\\\\\\\\Computer played s!\\\\\\\\")
+
+    def test_player_loses_scissors(self):
+        self.assertEqual(rock_paper_scissor.game_play(
+            's', 'r'), "You lost!!!\n\\\\\\\\Computer played r!\\\\\\\\")
+
+    def test_player_wins_scissors(self):
+        self.assertEqual(rock_paper_scissor.game_play(
+            's', 'p'), "You won!!!\nComputer played p!")
+
+    def test_player_tie_scissors(self):
+        self.assertEqual(rock_paper_scissor.game_play(
+            's', 's'), "It's a tie!!!Computer also played s!")
+
+    def test_player_no_input(self):
+        self.assertEqual(rock_paper_scissor.play(''), 'x')
 
 
 if __name__ == "__main__":
--- pythonProjects/rock-paper-scissor/rock_paper_scissor.py
+++ pythonProjects/rock-paper-scissor/rock_paper_scissor.py
@@ -1,24 +1,34 @@
 import random
 
 
-def play():
-    print("Are you ready to play Rock-Paper-Scissors with the computer?")
-    input("Hit ENTER to PLAY!")
-    player = input("Rock, Paper or Scissors?(r/p/s)? ").lower()
+def play(player):
+    if(player == '' or (player != 'r' and player != 'p' and player != 's')):
+        return 'x'
     computer = random.choice(['r', 'p', 's'])
-    game_play(player, computer)
+    print(game_play(player, computer))
 
 
 def game_play(player, computer):
+    # Is no input or invalid input-> exit game
+    if(player == '' or (player != 'r' and player != 'p' and player != 's')):
+        return "Invalid input!Try again!"
     # r > s, p > r, s > p
     if (player == 'r' and computer == 's') or (player == 'p' and computer == 'r') or (player == 's' and computer == 'p'):
-        print(f"You won!!!\nComputer played {computer}!")
+        return f"You won!!!\nComputer played {computer}!"
 
     elif (player == 'r' and computer == 'p') or (player == 'p' and computer == 's') or (player == 's' and computer == 'r'):
-        print(f"You lost!!!\n\\\\\\\\Computer played {computer}!\\\\\\\\")
+        return f"You lost!!!\n\\\\\\\\Computer played {computer}!\\\\\\\\"
 
     else:
-        print(f"It's a tie!!!Computer also played {computer}!")
+        return f"It's a tie!!!Computer also played {computer}!"
 
 
-play()
+if __name__ == "__main__":
+    print("*******ROCK  -  PAPER   -  SCISSORS********")
+    input("Hit ENTER to PLAY!")
+    player = 'x'
+    while(player == 'x'):
+        player = input("Rock, Paper or Scissors?(r/p/s)? ").lower()
+        if play(player) == 'x':
+            print("Invalid input, try again!")
+            player = 'x'
